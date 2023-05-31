59b8999f8c9f522e1233a08f924ae49bff637fc7
WolfZardST <85142764+WolfZardST@users.noreply.github.com>
2023-05-31 14:54:18 -0500

    Finishing Main Class

--- src/main/java/estimator/Main.java
+++ src/main/java/estimator/Main.java
@@ -1,6 +1,10 @@
 package estimator;
 
+import java.util.Scanner;
+
 public class Main {
+	
+	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
@@ -8,7 +12,14 @@ public class Main {
 		
 		System.out.println("Welcome to the E7 Vacation Package Cost Estimator\n");
 		
-		// TODO: request input and use Builder
+		System.out.println("Destination:\n");
+		while(!builder.setDestination(Main.scanner.nextLine()));
+		
+		System.out.println("Number of travelers:\n");
+		while(!builder.setNumberOfTravelers(Main.scanner.nextLine()));
+		
+		System.out.println("Duration (days):\n");
+		while(!builder.setDurationInDays(Main.scanner.nextLine()));
 		
 		VacationPackage vacationPackage = builder.getResult();
 		
