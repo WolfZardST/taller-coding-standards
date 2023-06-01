4b66e2b0607d33871565b55d710c52a57da1e048
rxcp02 <rxcabrer@espol.edu.ec>
2023-05-31 21:36:33 -0500

    apply checkstyle in VacationPackageCostCalculator

--- src/main/java/estimator/VacationPackageCostCalculator.java
+++ src/main/java/estimator/VacationPackageCostCalculator.java
@@ -2,54 +2,59 @@ package estimator;
 
 public class VacationPackageCostCalculator {
 	
-	private VacationPackage vacationPackage; 
+    private VacationPackage vacationPackage; 
+    /**
+	 * Only constructor for the VacationPackageCostCalculator object.
+	 */
+    public VacationPackageCostCalculator(final VacationPackage vacationPackage) {
+        this.vacationPackage = vacationPackage;
+    }
 	
-	public VacationPackageCostCalculator(VacationPackage vacationPackage) {
-		this.vacationPackage = vacationPackage;
-	}
-	
-	public void calculateCost() {
-		calculateWithDestination();
-		calculateWithNumberOfDays();
-		calculateWithNumberOfTravelers();
+    /**
+     * calculate the total cost of the package
+     */
+    public void calculateCost() {
+        calculateWithDestination();
+        calculateWithNumberOfDays();
+        calculateWithNumberOfTravelers();
 		
-	}
+    }
 	
-	private void calculateWithDestination() {
-		int cost = this.vacationPackage.getCost();
+    private void calculateWithDestination() {
+        int cost = this.vacationPackage.getCost();
 		
-		if (this.vacationPackage.getDestination().trim().toLowerCase().equals("paris")) {
-			this.vacationPackage.setCost(cost += 500);
-		}
-		else if (this.vacationPackage.getDestination().trim().toLowerCase().equals("new york")) {
-			this.vacationPackage.setCost(cost += 600);
-		}
-	}
+        if (this.vacationPackage.getDestination().trim().toLowerCase().equals("paris")) {
+            this.vacationPackage.setCost(cost += 500);
+        }
+        else if (this.vacationPackage.getDestination().trim().toLowerCase().equals("new york")) {
+            this.vacationPackage.setCost(cost += 600);
+        }
+    }
 	
-	private void calculateWithNumberOfTravelers() {
-		int cost = this.vacationPackage.getCost();
+    private void calculateWithNumberOfTravelers() {
+        int cost = this.vacationPackage.getCost();
 		
-		if (this.vacationPackage.getNumberOfTravelers() >= 4 && 
+        if (this.vacationPackage.getNumberOfTravelers() >= 4 && 
 			this.vacationPackage.getNumberOfTravelers() < 10) {
-			this.vacationPackage.setCost((int) Math.round(cost*0.9));
-		}
+            this.vacationPackage.setCost((int) Math.round(cost*0.9));
+        }
 		
-		if (this.vacationPackage.getNumberOfTravelers() > 10) {
-			this.vacationPackage.setCost((int) Math.round(cost*0.8));
-		}
-	}
+        if (this.vacationPackage.getNumberOfTravelers() > 10) {
+            this.vacationPackage.setCost((int) Math.round(cost*0.8));
+        }
+    }
 	
-	private void calculateWithNumberOfDays() {
-		int cost = this.vacationPackage.getCost();
+    private void calculateWithNumberOfDays() {
+        int cost = this.vacationPackage.getCost();
 		
-		if(this.vacationPackage.getDurationInDays() < 7) {
-			this.vacationPackage.setCost(cost += 200);
-		}
+        if(this.vacationPackage.getDurationInDays() < 7) {
+            this.vacationPackage.setCost(cost += 200);
+        }
 		
-		if(this.vacationPackage.getDurationInDays() >= 30 &&
+        if(this.vacationPackage.getDurationInDays() >= 30 &&
 			this.vacationPackage.getNumberOfTravelers() == 2) {
-			this.vacationPackage.setCost(cost -= 200);
-		}
-	}
+            this.vacationPackage.setCost(cost -= 200);
+        }
+    }
 	
 }
